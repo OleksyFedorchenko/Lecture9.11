eacee114282f2c0f8b6272b09880fe0dfe16418c
OleksyFedorchenko <otihiy@gmail.com>
2022-12-31 15:50:57 +0200

    adding initial and populate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into country(name) value('Ukraine');
+insert into country(name) value('Poland');
+insert into country(name) value('France');
+insert into country(name) value('Germany');
+insert into city(name,population,capital,country_id) values ('Lviv',34000,false,1);
+insert into city(name,population,capital,country_id) values ('Kyiv',58400,true,1);
