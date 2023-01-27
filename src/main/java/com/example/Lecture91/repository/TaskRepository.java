99fffb08c47fbc1367e9ca7167c5c0cbd2924bc5
OleksyFedorchenko <otihiy@gmail.com>
2023-01-27 18:34:55 +0200

    adding rest endpoint for lecture 12-13 ReactJS

--- src/main/java/com/example/Lecture91/repository/TaskRepository.java
+++ src/main/java/com/example/Lecture91/repository/TaskRepository.java
@@ -1,7 +1,6 @@
 package com.example.Lecture91.repository;
 
 import com.example.Lecture91.entity.TaskEntity;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
--- src/main/java/com/example/Lecture91/service/TaskServiceImpl.java
+++ src/main/java/com/example/Lecture91/service/TaskServiceImpl.java
@@ -1,11 +1,6 @@
 package com.example.Lecture91.service;
 
-
-import com.example.Lecture91.entity.TaskEntity;
 import com.example.Lecture91.repository.TaskRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,18 +8,11 @@ import java.util.List;
 @Service
 public class TaskServiceImpl implements TaskService {
     TaskRepository taskRepository;
-//    @PersistenceContext
-//    private EntityManager entityManager;
 
     public TaskServiceImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
 
     }
-
-//    @Override
-//    public List<String> findAllByLimit(Pageable pageable) {
-//        return taskRepository.findAllByLimit(pageable);
-//    }
 
     @Override
     public List<String> findAllRandomTasksByCount(int count) {
