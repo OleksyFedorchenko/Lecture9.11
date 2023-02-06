cce72c2d7f993b106a796fb434f171ab343d24f7
OleksyFedorchenko <otihiy@gmail.com>
2023-02-06 16:23:38 +0200

    refactoring code

--- src/main/java/com/example/Lecture91/repository/CountryRepository.java
+++ src/main/java/com/example/Lecture91/repository/CountryRepository.java
@@ -1,13 +1,10 @@
 package com.example.Lecture91.repository;
 
 import com.example.Lecture91.entity.CountryEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
-    CountryEntity findByName(String name);
+
 }
